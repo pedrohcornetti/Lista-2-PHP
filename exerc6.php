ff2563ccc9b632dccb2049928386adb9e01ba2eb
pedrohcornetti <pedrocornetti@outlook.com>
2024-04-18 22:54:17 -0300

    exercicios

---
+++ exerc6.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercícios</title>
+</head>
+<body>
+    <form action="exercicio6.php" method="post">
+        <label>Informe o valor A: </label>
+        <input type="number" name="valorA">
+        <button type="submit">Enviar</button>
+        <label>Informe o valor B: </label>
+        <input type="number" name="valorB">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ exerc7resposta.php
@@ -0,0 +1,6 @@
+<?php
+    $valor = $_POST['valor'];
+
+    $cm = $valor*100;
+    echo "$valor m em centímeros, equivale a : $cm cm";
+?>
